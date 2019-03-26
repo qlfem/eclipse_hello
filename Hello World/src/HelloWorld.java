170e0d71154dac6d5215b6ba0a7f576867baddbe
qlfem <qlfem@nate.com>
2019-03-26 17:34:52 +0900

    code

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -2,10 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
-<<<<<<< HEAD
 		System.out.println("Earth's End Dancehall");
-=======
-		System.out.println("Hello Dance World");
->>>>>>> refs/heads/hotfix
 	}
 }
